a55421fe02627865370d1c7436cd73b9a0dee296
Barri <info@bdesignstudio.co.uk>
2020-06-11 10:05:30 +0100

    Fetch data

--- app.js
+++ app.js
@@ -6,19 +6,24 @@ const searchUser = document.getElementById('searchUser');
 
 
 //-2 Search input  event listener
-searchUser.addEventListener( 'keyup', (e) => {
- //-3 Get input text in a var
+searchUser.addEventListener('keyup', (e) => {
+  //-3 Get input text in a var
   const userText = e.target.value;
-//-4 Check if user enter a text 
-  if(userText !== ''){
-//-9
+  //-4 Check if user enter a text 
+  if (userText !== '') {
+    //-9
     // Make http call
     github.getUser(userText)
-    .then(data =>{
-      console.log(data);
-    })
+      .then(data => {
+        // console.log(data);
+        if (data.profile.message === 'Not Found') {
+          // Show alert
+        } else {
+          //show profile
+        }
+      })
   } else {
-
+    // Clear profile
 
   }
-})
+});
--- github.js
+++ github.js
@@ -1,22 +1,21 @@
 //-5 making a class to send it to app.js
 
 class Github {
-  constructor(){
+  constructor() {
     //-6 We got them from github dev 
     this.client_id = '84dc4e3ff984f02b82fb';
     this.client_secret = '4ac6c9e8c0c349066df491a8a282bb0d3327d04e';
   }
-//-7 we are making get request with async
-//- 8 we are making two requests -one for user one for repo
-async getUser(user){
-  //-user
-  const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-  //-repo
-  const profile = await profileResponse.json();
-  //- we use promise to get object faster
-  return {
-    profile
+  //-7 we are making get request with async
+  //- 8 we are making two requests -one for user one for repo
+  async getUser(user) {
+    //-user
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    //-repo
+    const profile = await profileResponse.json();
+    //- we use promise to get object faster
+    return {
+      profile
+    }
   }
-}
-
 }
